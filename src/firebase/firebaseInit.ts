54d04f74d392c12a9507ac5c7e0f3be64f830086
kevinqtogitty <103936802+kevinqtogitty@users.noreply.github.com>
2023-05-04 15:15:34 -0400

    Initial commit

---
+++ src/firebase/firebaseInit.ts
@@ -0,0 +1,15 @@
+import dotenv from 'dotenv';
+dotenv.config();
+
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+export const firebaseConfig = {
+  apiKey: process.env.APIKEY,
+  authDomain: process.env.AUTHDOMAIN,
+  projectId: process.env.PROJECTID,
+  storageBucket: process.env.STORAGEBUCKET,
+  messagingSenderId: process.env.MESSAGINGSENDERID,
+  appId: process.env.APPID
+};
