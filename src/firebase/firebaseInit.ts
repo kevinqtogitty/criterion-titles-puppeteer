224b7b5cb6913dba6700d447dd0812737483a359
kevinqtogitty <103936802+kevinqtogitty@users.noreply.github.com>
2023-06-12 15:03:21 -0400

    Update: fixed env names

--- src/firebase/firebaseInit.ts
+++ src/firebase/firebaseInit.ts
@@ -6,10 +6,10 @@ dotenv.config();
 
 // Your web app's Firebase configuration
 export const firebaseConfig = {
-  apiKey: process.env.APIKEY,
-  authDomain: process.env.AUTHDOMAIN,
-  projectId: process.env.PROJECTID,
-  storageBucket: process.env.STORAGEBUCKET,
-  messagingSenderId: process.env.MESSAGINGSENDERID,
-  appId: process.env.APPID
+  apiKey: process.env.API_KEY,
+  authDomain: process.env.AUTH_DOMAIN,
+  projectId: process.env.PROJECT_ID,
+  storageBucket: process.env.STORAGE_BUCKET,
+  messagingSenderId: process.env.MESSAGING_SENDER_ID,
+  appId: process.env.APP_ID
 };
